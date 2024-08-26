8b9f175b64ec19154733206939fd9ef6044d718c
Max Greenwald <maxsupergreenwald@gmail.com>
2024-08-26 10:09:22 -0400

    updated script.js and labjswrapper.js to have a uniquely named taskData object to prevent cross saving

--- public/script.js
+++ public/script.js
@@ -495,9 +495,9 @@ function startExperiment() {
         // Implement data submission to server here
         console.log('Experiment complete. Data:', experimentData);
         const trialsDataJson = JSON.stringify(experimentData);
-        sessionStorage.setItem('taskData', trialsDataJson);
+        sessionStorage.setItem('fishertaskd3taskData', trialsDataJson);
         window.postMessage({
-            type: 'labjs.data',
+            type: 'fishertaskd3labjs.data',
             json: trialsDataJson
         }, '*');
     }
